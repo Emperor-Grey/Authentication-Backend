58763ce3426bd1319b66beab09a34d790e986169
King Grey <kinggrey9959@gmail.com>
2024-02-13 15:35:39 +0530

    Added ejs

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const collection = require('./src/config/db.js');
-const userRoutes = require('./src/routes/auth.js');
+const express = require("express");
+const collection = require("./src/config/db.js");
+const path = require("path");
+const userRoutes = require("./src/routes/auth.js");
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src", "views"));
 app.use(express.urlencoded({ urlencoded: false }));
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 
-app.get('/', (req, res) => {
-  console.log('Home router has been reached');
-  res.send('HOME PAGE');
+app.get("/", (req, res) => {
+  console.log("Home router has been reached");
+  res.send("HOME PAGE");
 });
 
 app.listen(PORT, () =>
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const { RegisterUser, LoginUser } = require('../controllers/Auth');
+const express = require("express");
+const { RegisterUser, LoginUser } = require("../controllers/Auth");
 
 const router = express.Router();
 
-router.post('/register', RegisterUser);
-router.post('/login', LoginUser);
+router.post("/register", RegisterUser);
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+router.post("/login", LoginUser);
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
 module.exports = router;
